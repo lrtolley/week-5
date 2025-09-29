debc4b5719170b8b53ef4d966730768c8d55af14
Lauren T <ltolley@iu.edu>
2025-09-29 20:44:41 +0000

    inital commit message - fixing interpreter issue

--- apputil.py
+++ apputil.py
@@ -1,4 +1,10 @@
 import plotly.express as px
 import pandas as pd
+#import plotly.io as pio
+#pio.renderers.default = 'iframe'
 
 # update/add code below ...
+
+titanic_df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
+
+print(titanic_df.head())
