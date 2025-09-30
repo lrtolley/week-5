352a6080f0ae0860df5c2305db4b39a37315a655
Lauren T <ltolley@iu.edu>
2025-09-30 15:49:57 +0000

    in progress building histogr

--- apputil.py
+++ apputil.py
@@ -7,4 +7,18 @@ import pandas as pd
 
 titanic_df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
-print(titanic_df.head())
+def survival_demographics():
+    ''' Defined a function to analyze survival demographics on the Titanic. '''
+    bins = [0, 12, 19, 59, 1000]
+    labels = ['Child', 'Teen', 'Adult', 'Senior']
+    titanic_df['Age Group'] = pd.cut(titanic_df['Age'], bins=bins, labels=labels, include_lowest=True)
+
+fig = px.histogram(titanic_df, 
+                   x='Age', 
+                   color='Survived',
+                   template='plotly_white',
+                   color_discrete_sequence=px.colors.qualitative.D3
+                  )
+fig.update_layout(title='Titanic Age Distribution by Survival Status',)
+fig.show()
+
